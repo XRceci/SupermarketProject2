d4b8d09301d83ea0dc5515cdd373469d35f39fb1
Thanaell <gaelle_clavelin@orange.com>
2024-01-11 22:23:38 +0100

    Changing project structure

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour {
+    [SerializeField]
+    private MeshRenderer arrowMesh;
+
+    [SerializeField]
+    private TaskManager taskManager;
+
+    void Start () {
+        // Needed?
+    }
+
+	void Update () {
+        SelectableObject currentSelectableObject = taskManager.getCurrentSelectableObject();
+        if (currentSelectableObject != null)
+        {
+            this.transform.LookAt(currentSelectableObject.transform.position);
+        }
+	}
+}
---
+++ Assets/Scripts/SelectableObject.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectableObject : MonoBehaviour
+{
+
+    public bool isSelected = false;
+    public Material matSelection;
+    public Material originalMaterial;
+
+    TaskManager manager;
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        manager = GameObject.Find("Models").GetComponent<TaskManager>();
+        try
+        {
+            originalMaterial = this.GetComponent<MeshRenderer>().material;
+            matSelection = manager.materialSelection;
+        }
+        catch(Exception ex)
+        {
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void HighlightObject()
+    {
+        if (matSelection)
+        {
+            this.GetComponent<MeshRenderer>().material = matSelection;
+            isSelected = true;
+        }
+    }
+
+    public void SelectObject()
+    {
+        if (matSelection)
+        {
+            if (manager.isCurrentSelectableObject(this.name))
+            {
+                this.GetComponent<MeshRenderer>().material = originalMaterial;
+                isSelected = true;
+                manager.notifySelection(true);
+                print("right stuff");
+            }
+            else
+            {
+                manager.notifySelection(false);
+                print("wrong stuff");
+                //play sound?
+            }
+            //manager.SendMessage("isSelectedObjectSelected", this.gameObject.name);
+        }
+    }
+
+    public void deSelectObject()
+    {
+        this.GetComponent<MeshRenderer>().material = originalMaterial;
+        isSelected = false;
+    }
+}
